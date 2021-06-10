ccd45f4c97ab8bec478a0e41b378b4f4c1226f97
GustavoHBS <gustavo56c@gmail.com>
2021-06-09 21:02:38 -0300

    Criando interfaces do exame toxicologico

---
+++ src/exam/services/toxicologicalService.interface.ts
@@ -0,0 +1,5 @@
+import { IToxicologicalSample } from '../interfaces/toxicologicalSample.interface';
+
+export interface IToxicologicalService {
+  processExam(sample: IToxicologicalSample): Promise<any>;
+}
