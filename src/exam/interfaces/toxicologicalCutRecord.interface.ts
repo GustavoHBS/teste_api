b7d00104c9a636045fcc7fe7d8ff83bc771f09f9
GustavoHBS <gustavo56c@gmail.com>
2021-06-09 21:02:56 -0300

    Criando interface da nota de corte

---
+++ src/exam/interfaces/toxicologicalCutRecord.interface.ts
@@ -0,0 +1,11 @@
+export interface IToxicologicalCutRecord {
+  Cocaína: number;
+  Anfetamina: number;
+  Metanfetamina: number;
+  MDA: number;
+  MDMA: number;
+  THC: number;
+  Morfina: number;
+  Codeína: number;
+  Heroína: number;
+}
