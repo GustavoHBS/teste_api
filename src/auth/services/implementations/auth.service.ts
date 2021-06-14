1ae25bed03207daf8aeb7f2865b78ab337b859dd
GustavoHBS <gustavo56c@gmail.com>
2021-06-14 01:03:50 -0300

    Alterando variavel da porta e do tempo de expiração

--- src/auth/services/implementations/auth.service.ts
+++ src/auth/services/implementations/auth.service.ts
@@ -20,8 +20,10 @@ export class AuthService {
 
   async login(user: UsersDocument) {
     const payload = { username: user.username, sub: user._id };
+    const MILISECONDS_CALC = 10;
     return {
       access_token: this.jwtService.sign(payload),
+      expiresIn: parseInt(process.env.EXPIRATION_SECONDS) * MILISECONDS_CALC,
     };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.PORT;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(SERVER_PORT, () => {
