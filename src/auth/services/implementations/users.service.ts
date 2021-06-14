84f2e405d2b4a4052591bab46e25926f3a8e92a2
GustavoHBS <gustavo56c@gmail.com>
2021-06-14 01:04:01 -0300

    Adicionando tratativas de erro

--- src/auth/services/implementations/users.service.ts
+++ src/auth/services/implementations/users.service.ts
@@ -1,3 +1,7 @@
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Inject, Injectable } from '@nestjs/common';
 import { IUsersModel } from 'src/auth/models/usersModel.interface';
 import { UsersDocument } from 'src/base/database/entities/users.entity';
@@ -10,7 +14,23 @@ export class UsersService {
     return this.usersModel.findByUsername(username);
   }
 
-  async create(username: string, password: string): Promise<UsersDocument> {
-    return this.usersModel.create(username, password);
+  async create(username: string, password: string): Promise<any> {
+    return this.usersModel
+      .create(username, password)
+      .then(() => {
+        return {
+          message: 'Usuário criado com sucesso',
+        };
+      })
+      .catch(err => {
+        if (err.code === 11000) {
+          throw new BadRequestException({
+            message:
+              'Já existe um usuário com este username, tente novamente com outro username.',
+          });
+        } else {
+          throw new InternalServerErrorException();
+        }
+      });
   }
 }
