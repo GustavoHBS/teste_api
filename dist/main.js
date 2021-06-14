6114d21c2f6769baf730190e033d2466ee75e202
GustavoHBS <gustavo56c@gmail.com>
2021-06-14 00:36:19 -0300

    Alterando porta para pegar a padrão

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.PORT;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(SERVER_PORT, () => {
